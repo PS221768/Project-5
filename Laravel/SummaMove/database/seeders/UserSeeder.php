595e0c73a8211db402af685d0ae24534a6b19645
Xander van Hunen <ps221768@edu.summacollege.nl>
2023-06-12 11:53:07 +0200

    Add migrations and seeders, and fix common issue 1071
    // App
    Modified: Laravel/SummaMove/app/Providers/AppServiceProvider.php ; to fix a common issue 1071 with key length limitation
    
    // Migrations
    Added new migration file: Laravel/SummaMove/database/migrations/2023_06_12_093616_achievements.php
    Added new migration file: Laravel/SummaMove/database/migrations/2023_06_12_093619_exercises.php
    
    Implements: #2
    
    // Seeders
    Added new seeder file: Laravel/SummaMove/database/seeders/ExerciseSeeder.php
    Added new seeder file: Laravel/SummaMove/database/seeders/AchievementSeeder.php
    Added new seeder file: Laravel/SummaMove/database/seeders/UserSeeder.php
    
    Modified: Laravel/SummaMove/database/seeders/DatabaseSeeder.php ; to make all seeders able to fire of with the default command 'php artisan migrate:fresh --seed'
    
    Implements: #3

---
+++ Laravel/SummaMove/database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon as carbon;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // DB::table('users')->truncate();
+        DB::table('users')->insert([
+            'name' => "Jan Jansen",
+            'email' => "JJ@gmail.com",
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make("password"),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => "Lam Lamsen",
+            'email' => "LL@gmail.com",
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make("password"),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+    }
+}
